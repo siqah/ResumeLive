c0cafbd949d490090be76095a8aba4ecfc6ffd6c
Faruoq muhammed Masika <149321792+siqah@users.noreply.github.com>
2024-12-31 22:32:15 +0000

    Routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
 
-function App() {
+import {BrowserRouter,Routes, Route} from 'react-dom'
 
+import Dashboard from './pages/Dashboard'
+import LandingPage from './pages/LandingPage'
+import ResumeBuilder from './pages/ResumeBuilder'
+import ProfileSettings from './pages/ProfileSettings'
+
+const App = () => {
   return (
-    <>
-     <p>Hello</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/landingpage" element={< LandingPage/>} />
+        <Route path="/resumebuilder" element={<ResumeBuilder />} />
+        <Route path="/profilesettings" element={<ProfileSettings />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
